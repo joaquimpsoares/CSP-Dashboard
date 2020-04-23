3022a8bf0f588f40ab5c421f109d1f5e637e3913
Marco Aurelio <marco.aurelio@tagydes.com>
2020-04-23 15:29:10 +0200

    MDB Installed

--- public/js/app.js
+++ public/js/app.js
@@ -68302,8 +68302,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Dev\clean-dashboard\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Dev\clean-dashboard\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/kali/Documents/PHP/Tagydes/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/kali/Documents/PHP/Tagydes/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
