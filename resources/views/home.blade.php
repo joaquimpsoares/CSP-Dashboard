1a338d6f23d1582f38c7a331e2d21b1914087f76
Site Tagydes <joaquim.soares@tagydes.com>
2020-05-01 22:12:21 +0200

    change home to microsoft card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,9 +14,9 @@
                             <div class="jumbotron card card-image" style="background-image: url(https://mdbootstrap.com/img/Photos/Others/gradient1.jpg);">
                                 <div class="text-white text-center py-5 px-4">
                                     <div>
-                                        <h2 class="card-title h1-responsive pt-3 mb-5 font-bold"><strong>Create your beautiful website with MDBootstrap</strong></h2>
-                                        <p class="mx-5 mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat fugiat, laboriosam, voluptatem,
-                                            optio vero odio nam sit officia accusamus minus error nisi architecto nulla ipsum dignissimos. Odit sed qui, dolorum!
+                                        <h2 class="card-title h1-responsive pt-3 mb-5 font-bold"><strong>Sell to your customers</strong></strong></h2>
+                                        <p class="mx-5 mb-5">With the automatic provision for Microsoft provide fast subscriptions.Our tool allows you to sell to your customers
+                                            and easy your life.
                                         </p>
                                         <a class="btn btn-outline-white btn-md"><i class="fas fa-clone left"></i> View project</a>
                                     </div>
@@ -25,14 +25,13 @@
                             <div class="row">
                                 <div class="col-md-4">
                                     <div class="card">
-                                        <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" alt="Card image cap">
+                                        <img class="card-img-top" src="https://img.pngio.com/microsoft-corporate-logo-guidelines-trademarks-microsoft-logo-png-2008_900.jpg" alt="Card image cap">
                                         <div class="card-body">
-                                            <h4 class="card-title"><a>Card title</a></h4>
-                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                                                content.
+                                            <h4 class="card-title"><a>Microsoft Products</a></h4>
+                                            <p class="card-text">How automation will provide fast deployment of Microsoft subscriptions.
                                             </p>
-                                            <a href="#" class="btn btn-primary">
-                                                Button
+                                            <a href="#" class="button is-outlined is-primary">
+                                                Read more
                                             </a>
                                         </div>
                                     </div>
