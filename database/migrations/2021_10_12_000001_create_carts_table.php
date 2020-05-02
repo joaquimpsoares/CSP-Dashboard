adbd3ae918227432ef5ec431e4bb74f83e376e68
Marco Aurelio <marco.aurelio@tagydes.com>
2020-05-02 22:39:34 +0200

    Migrated Cart from Session to Database

---
+++ database/migrations/2021_10_12_000001_create_carts_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('customer_id')->nullable();
+            $table->string('domain')->nullable();
+            $table->unsignedInteger('user_id');
+            $table->uuid('token')->unique();
+            $table->boolean('verify')->nullable();
+            $table->boolean('verified')->nullable();
+            $table->text('comments')->nullable();
+            $table->timestamps();
+
+            $table->foreign('customer_id')->references('id')->on('customers');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carts');
+    }
+}
