3ce442a46605d229643d5d305732b68a688efd0a
Site Tagydes <joaquim.soares@tagydes.com>
2020-06-04 17:02:59 +0200

    add card to subscription view for customer

--- resources/views/subscriptions/index.blade.php
+++ resources/views/subscriptions/index.blade.php
@@ -11,6 +11,10 @@
 				<i class="fas fa-file-invoice-dollar fa-lg primary-color z-depth-2 p-4 ml-2 mt-n3 rounded text-white"></i>
 				<div class="card-body">
                     <h4 class="card-title">{{ ucwords(trans_choice('messages.subscription_table', 2)) }} </h4>
+					{{-- {{dd(Auth::user()->userLevel->name == "Customer")}} --}}
+					@if (Auth::user()->userLevel->name == "Customer")
+					@include('subscriptions.partials.card', ['subscriptions' => $subscriptions])
+					@endif
 					@include('subscriptions.partials.table', ['subscriptions' => $subscriptions])
 				</div>
 			</div>
