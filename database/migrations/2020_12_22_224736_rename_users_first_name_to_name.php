af362dbc994ab150ad697d6fe4b18da7fee07d49
Joaquim Soares <joaquim.soares@tagydes.com>
2020-12-23 00:25:43 +0100

    change users first_name to name in the table

--- database/migrations/2020_12_22_224736_rename_users_first_name_to_name.php
+++ database/migrations/2020_12_22_224736_rename_users_first_name_to_name.php
@@ -14,7 +14,7 @@ class RenameUsersFirstNameToName extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->renameColumn('name', 'name');
+            $table->renameColumn('first_name', 'name');
         });
 
     }
