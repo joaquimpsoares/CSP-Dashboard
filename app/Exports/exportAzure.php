7f085e2d4b7f415f6d00b8dfece2cc180c2d0e47
Joaquim Soares <joaquim.soares@tagydes.com>
2021-02-05 15:44:30 +0100

    add heading to export excel

--- app/Exports/exportAzure.php
+++ app/Exports/exportAzure.php
@@ -6,12 +6,43 @@ use App\Models\Student;
 use App\Models\AzureUsageReport;
 use Maatwebsite\Excel\Concerns\FromQuery;
 use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
 
-class exportAzure implements
-    FromQuery
+class exportAzure implements FromQuery, WithHeadings
+
 {
     use Exportable;
+
+    public function headings(): array
+    {
+        return [
+            '#',
+            'subscription_id',
+            'Resource Group',
+            'resource_location',
+            'resource_id',
+            'resource_name',
+            'resource_category',
+            'resource_subcategory',
+            'resource_region',
+            'quantity',
+            'unit',
+            'ext_order_id',
+            'usageStartTime',
+            'usageEndTime',
+            'cost',
+            'created_at',
+            'updated_at',
+            'resourceType',
+            'usageResourceKind',
+            'dataCenter',
+            'networkBucket',
+            'pipelineType',
+            'name',
+        ];
+    }
+
     protected $students;
 
     public function __construct($students)
