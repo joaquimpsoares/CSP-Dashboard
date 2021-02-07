3afcb05b231ab05b476b1dfb80b0eac966caaf3e
Joaquim Soares <joaquim.soares@tagydes.com>
2021-02-07 14:12:57 +0100

    changes to cunstruct

--- app/Console/Commands/syncAzureBudget.php
+++ app/Console/Commands/syncAzureBudget.php
@@ -3,48 +3,55 @@
 namespace App\Console\Commands;
 
 use App\Subscription;
-use Illuminate\Console\Command;
 use App\Repositories\AnalyticRepositoryInterface;
+use Illuminate\Console\Command;
 
-class syncAzureBudget extends Command
+class SyncAzureBudget extends Command
 {
 
-    public function __construct(
-        AnalyticRepositoryInterface $analyticRepository
-        ) {
-            $this->analyticRepository = $analyticRepository;
-        }
 
-        /**
-        * The name and signature of the console command.
-        *
-        * @var string
-        */
-        protected $signature = 'syncAzureBudget:daily';
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'SyncAzureBudget:daily';
 
-        /**
-        * The console command description.
-        *
-        * @var string
-        */
-        protected $description = 'Command description';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
 
-        /**
-        * Execute the console command.
-        *
-        * @return int
-        */
-        public function handle()
-        {
-            $subscriptions = Subscription::where('billing_type', 'usage')->get();
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct(AnalyticRepositoryInterface $analyticRepository)
+    {
+        parent::__construct();
+        $this->analyticRepository = $analyticRepository;
 
-
-            foreach($subscriptions as $subscription){
-                $msId = $subscription->customer->microsoftTenantInfo->first()->tenant_id;
-                $details = $this->analyticRepository->UpdateAZURE($msId, $subscription);
-
-                // dd($details);
-
-            }
-        }
     }
+
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $subscriptions = Subscription::where('billing_type', 'usage')->get();
+
+
+    foreach($subscriptions as $subscription){
+            $msId = $subscription->customer->microsoftTenantInfo->first()->tenant_id;
+            $details = $this->analyticRepository->UpdateAZURE($msId, $subscription);
+    }
+
+    }
+}
+
