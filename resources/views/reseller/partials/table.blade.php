d95aed220426fe2a10f4bbffdefc9841b69d1f31
Joaquim Soares <joaquim.soares@tagydes.com>
2021-03-05 11:06:40 +0100

    reseller table mpnid show

--- resources/views/reseller/partials/table.blade.php
+++ resources/views/reseller/partials/table.blade.php
@@ -2,16 +2,16 @@
     <div class="card-header">
         <h3 class="card-title">{{ ucwords(trans_choice('messages.reseller_table', 2)) }}</h3>
         <div class="card-options">
-            <div class="btn-group ml-5 mb-0">
+            <div class="mb-0 ml-5 btn-group">
                 <button type="button" class="btn btn-white dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fe fe-plus"></i> {{ ucwords(__('messages.options')) }}</button>
                 <div class="dropdown-menu">
                     @if(Auth::user()->userLevel->id === 3)
-                    <a class="dropdown-item" href="{{route('reseller.create')}}"><i class="fa fa-plus mr-2"></i>{{ ucwords(__('messages.new_reseller')) }}</a>
+                    <a class="dropdown-item" href="{{route('reseller.create')}}"><i class="mr-2 fa fa-plus"></i>{{ ucwords(__('messages.new_reseller')) }}</a>
                     @endif
-                    {{-- <a class="dropdown-item" href="#"><i class="fa fa-eye mr-2"></i>View all new tab</a>
-                    <a class="dropdown-item" href="#"><i class="fa fa-edit mr-2"></i>Edit Page</a>
+                    {{-- <a class="dropdown-item" href="#"><i class="mr-2 fa fa-eye"></i>View all new tab</a>
+                    <a class="dropdown-item" href="#"><i class="mr-2 fa fa-edit"></i>Edit Page</a>
                     <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#"><i class="fa fa-cog mr-2"></i> Settings</a> --}}
+                    <a class="dropdown-item" href="#"><i class="mr-2 fa fa-cog"></i> Settings</a> --}}
                 </div>
             </div>
         </div>
@@ -40,15 +40,15 @@
                             <td>{{ $reseller['customers'] }}</td>
                             <td><a href="{{$reseller['provider']->format()['path']}}">{{ $reseller['provider']['company_name'] }}</td>
                                 <td>{{ $reseller['country'] }}</td>
-                                @if($reseller['provider']['instances']->first()['external_type'] == 'indirect')
+                                {{-- @if($reseller['provider']['instances']->first()['external_type'] == 'indirect') --}}
                                 <td>{{ $reseller['mpnid'] }}</td>
-                                @else
-                                <td></td>
-                                @endif
+                                {{-- @else --}}
+                                {{-- <td></td>
+                                @endif --}}
                                 <td>{{ $reseller['created_at'] }}</td>
                                 <td style="width: 150px">
                                     <div class="row text-nowrap">
-                                        <div class="btn-group align-top">
+                                        <div class="align-top btn-group">
                                             @if(Auth::user()->can('edit'))
                                             <a href="{{$reseller['path']}}/edit" class="btn btn-sm btn-white btn-svg">{{ ucwords(trans_choice('messages.edit', 1)) }} </a>
                                             {{-- <button class="btn btn-sm btn-white btn-svg" type="button"><svg class="svg-icon" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M8 9h8v10H8z" opacity=".3"/><path d="M15.5 4l-1-1h-5l-1 1H5v2h14V4zM6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM8 9h8v10H8V9z"/></svg></button> --}}
