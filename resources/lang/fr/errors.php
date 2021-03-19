169fe4d0795ea7dcee8840a217d86e43e97bc863
Joaquim Soares <joaquim.soares@tagydes.com>
2021-03-19 09:30:43 +0100

    translations

--- resources/lang/fr/errors.php
+++ resources/lang/fr/errors.php
@@ -1,8 +1,7 @@
 <?php 
 return [
-  'unauthorized_action' => '',
-  'access_with_resellers_credentials' => '',
-  'user_already_exists' => '',
-  'error' => '',
-  '' => '',
+  'unauthorized_action' => 'Action non autorisée',
+  'access_with_resellers_credentials' => 'Accès avec des références de revendeurs',
+  'user_already_exists' => 'l\'utilisateur existe déjà',
+  'error' => 'Erreur',
 ];
