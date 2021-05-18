1fa21a255d0fe509e1ad8bdc815638a8059ed9a2
Sergio Rodenas <rodenastyle@gmail.com>
2021-05-18 18:41:08 +0000

    Restore product_id by instance new command

--- app/Console/Commands/MovePriceSkuToId.php
+++ app/Console/Commands/MovePriceSkuToId.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use App\Product;
+use App\Price;
 
 class MovePriceSkuToId extends Command
 {
@@ -14,10 +15,10 @@ class MovePriceSkuToId extends Command
      * We'll do the migration in 2 steps:
      * 1.- Create the product_id field on the prices table as nullable so we can safely update all the rows (this commit)
      * 2.- Change the relationship method on the price model, remove product_sku field and make product_id non-nullable (non-commited)
-     * 
+     *
      * Things noticed in the process:
      * - The product() method on Price model (relationship method) depends on session() so cannot be used in this context and SHOULD be refactored
-     * 
+     *
      * Things after migration: (non-commited)
      * - Metadata table and Trait with polymorphic relationship ->meta() method to apply on Models (Make it magic, string param means get, array param means set)
      * - Add is_perpetual field to products
@@ -26,10 +27,9 @@ class MovePriceSkuToId extends Command
 
     public function handle()
     {
-        Product::eachById(function(Product $product){
-            $product->prices()->update([
-                'product_id' => $product->id
-            ]);
+        Price::eachById(function(Price $price){
+            $product = Product::where('instance_id', $price->instance_id)->where('sku', $price->product_sku)->first();
+            $price->update(['product_id' => $product->id]);
         });
 
         return 0;
