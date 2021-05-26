68aa160bac746925b9bb53bb024c25048c4ea914
Sergio Rodenas <rodenastyle@gmail.com>
2021-05-26 11:35:45 +0000

    Add RedirectIfAuthenticated middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,6 +38,7 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\CheckBlocked::class,
             \App\Http\Middleware\LocaleDetector::class,
+            \App\Http\Middleware\RedirectIfAuthenticated::class,
         ],
 
         'api' => [
