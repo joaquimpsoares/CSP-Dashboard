390f842f7f453d44969e454f032cb4ee97fce37a
Sergio Rodenas <rodenastyle@gmail.com>
2021-05-26 11:22:58 +0000

    Remove the final symbol

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -75,7 +75,7 @@ class LoginController extends Controller
             return Redirect::route('login')->with('danger','Please ask for the correct permissions to access the app: ');
         }else {
             Auth::login($user, true);
-            return redirect()->route('home');
+            return redirect()->route('home')->withoutFragment();
         }
     }
 
