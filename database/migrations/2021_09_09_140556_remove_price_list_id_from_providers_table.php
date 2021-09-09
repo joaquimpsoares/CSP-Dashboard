64104c4f01efd336f8468c1e2b21729557b91cff
Sergio Rodenas <rodenastyle@gmail.com>
2021-09-09 16:07:49 +0200

    Fix migration (add foreign)

--- database/migrations/2021_09_09_140556_remove_price_list_id_from_providers_table.php
+++ database/migrations/2021_09_09_140556_remove_price_list_id_from_providers_table.php
@@ -14,6 +14,7 @@ class RemovePriceListIdFromProvidersTable extends Migration
     public function up()
     {
         Schema::table('providers', function (Blueprint $table) {
+            $table->dropForeign('providers_price_list_id_foreign');
             $table->dropColumn('price_list_id');
         });
     }
