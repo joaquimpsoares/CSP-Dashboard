4105ef90db3fe40b1e79366d98bf45696ec136bd
joaquim soares <joaquim.soares@tagydes.com>
2021-12-20 22:09:48 +0100

    changes to billed command

--- Modules/AzureAnalytics/Console/AzureBilledCommand.php
+++ Modules/AzureAnalytics/Console/AzureBilledCommand.php
@@ -48,30 +48,31 @@ class AzureBilledCommand extends Command
     */
     public function handle()
     {
-        Mail::raw("Starting Azure Syncronization", function ($mail)  {
-            $mail->to('joaquim.soares@tagydes.com')
-            ->subject('Daily importing Started Azure reports');
-        });
+        // Mail::raw("Starting Azure Syncronization", function ($mail)  {
+        //     $mail->to('joaquim.soares@tagydes.com')
+        //     ->subject('Daily importing Started Azure reports');
+        // });
 
         MsftInvoices::eachById(function (MsftInvoices $invoice) {
-                if(substr($invoice->invoice_id,0,1) === "G"){
+            if(substr($invoice->invoice_id,0,3) === "D05"){
                     try {
                         $resources = FacadesAzureResource::withCredentials($invoice->instance->external_id,$invoice->instance->external_token)
-                        ->invoicebilled($invoice->provider->country->currency_code,$invoice->invoice_id);
+                        ->invoicerecon($invoice->provider->country->currency_code,$invoice->invoice_id);
                         Log::channel('azure')->info($invoice->invoice_id);
-            $this->info('Updating Invoice ID '.$invoice->invoice_id);
-
+                    $this->info('Updating Invoice ID '.$invoice->invoice_id);
                     } catch (\Throwable $th) {
-                        Mail::raw($th->getMessage(), function ($mail) use($th) {
-                            $mail->to('joaquim.soares@tagydes.com')
-                            ->subject('Failed importing');
-                        });
+                        $this->info('error updating Invoice ID '.$th->getMessage());
+                        // Mail::raw($th->getMessage(), function ($mail) use($th) {
+                        //     $mail->to('joaquim.soares@tagydes.com')
+                        //     ->subject('Failed importing');
+                        // });
                     }
                     $Count = 0;
 
                     if($resources){
                         foreach ($resources as $key => $value) {
                             foreach ($value as $key => $value) {
+                                // dd($value);
                                 $tenant = MicrosoftTenantInfo::where('tenant_id', $value['customerId'])->first();
                                 if(isset($tenant->customer)){
                                     $tenant->customer->subscriptions->where('billing_type', 'usage')->each(function ($subscription) use($value,$Count) {
@@ -79,35 +80,35 @@ class AzureBilledCommand extends Command
                                             $Count++;
                                             $resource = AzureUsageReport::updateOrCreate([
                                                 'subscription_id'       => $subscription->id,
-                                                'resource_id'           => $value['productId'],
-                                                'name'                  => $value['productName'],
-                                                'resource_name'         => $value['productName'],
+                                                'unitPrice'             => $value['listPrice'],
+                                                'resource_id'           => $value['resourceGuid'],
+                                                'name'                  => $value['serviceName'],
+                                                'resource_name'         => $value['resourceName'],
                                                 'resource_group'        => $value['resourceGroup'],
-                                                'resource_location'     => $value['resourceLocation'],
-                                                'resource_region'       => $value['meterRegion'],
+                                                // 'resource_location'     => $value['region'],
+                                                'resource_region'       => $value['region'],
                                                 'resource_category'     => $value['meterCategory'],
                                                 'usageStartTime'        => $value['chargeStartDate'],
                                                 'usageEndTime'          => $value['chargeEndDate'],
-                                                'resource_subcategory'  => $value['meterSubCategory'],
+                                                // 'resource_subcategory'  => $value['meterSubCategory'],
                                             ], [
                                                 'usagedate'             => $value['usageDate'],
-                                                'quantity'              => $value['quantity'],
-                                                'unit'                  => $value['unitOfMeasure'],
+                                                'quantity'              => $value['consumedQuantity'],
+                                                'unit'                  => $value['unit'],
                                                 'cost'                  => $value['billingPreTaxTotal'],
                                                 'resourceType'          => $value['meterType'],
-                                                'tags'                  => $value['tags'],
-                                                'unitPrice'             => $value['unitPrice'],
-                                                'additionalinfo'        => $value['additionalInfo'],
+                                                // 'tags'                  => $value['tags'],
+                                                // 'additionalinfo'        => $value['additionalInfo'],
                                             ]);
                                             $this->info('updating '.$resource->resource_name . ''. $resource->usagedate );
                                             Log::channel('azure')->info('updated '.$resource->resource_name);
                                         }
                                         catch (\Exception $e) {
                                             Log::channel('azure')->info($e->getMessage());
-                                            Mail::raw($e->getMessage(), function ($mail) use($e) {
-                                                $mail->to('joaquim.soares@tagydes.com')
-                                                ->subject('Azure Sync Failed');
-                                            });
+                                            // Mail::raw($e->getMessage(), function ($mail) use($e) {
+                                            //     $mail->to('joaquim.soares@tagydes.com')
+                                            //     ->subject('Azure Sync Failed');
+                                            // });
                                         }
                                     });
                                 }
@@ -117,10 +118,10 @@ class AzureBilledCommand extends Command
                 }
             });
 
-            Mail::raw("Just finished Azure Syncronization", function ($mail)  {
-                $mail->to('joaquim.soares@tagydes.com')
-                ->subject('Daily imported Azure reports');
-            });
+            // Mail::raw("Just finished Azure Syncronization", function ($mail)  {
+            //     $mail->to('joaquim.soares@tagydes.com')
+            //     ->subject('Daily imported Azure reports');
+            // });
 
             $this->info('Successfully sent daily quote to everyone.');
 
