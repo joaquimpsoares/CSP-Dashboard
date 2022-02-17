22846e87c16d07754818d6904c75b0e98c80159d
joaquim soares <joaquim.soares@tagydes.com>
2022-02-17 17:15:45 +0100

    changes to tenantinfog

--- resources/views/livewire/customer/show-customer.blade.php
+++ resources/views/livewire/customer/show-customer.blade.php
@@ -1,4 +1,5 @@
-<div wire:init="checkQualificationStatus({{ $customer->id }})">
+
+<div @if($customer->microsoftTenantInfo->first())  wire:init="checkQualificationStatus({{ $customer->id }}) @endif">
     <script src="https://unpkg.com/@popperjs/core@2"></script>
     <script src="https://unpkg.com/tippy.js@6"></script>
     <!-- This example requires Tailwind CSS v2.0+ -->
@@ -48,28 +49,6 @@
                                 {{ ucwords(trans_choice($customer->status->name, 1)) }}
                             </span>
                         </div>
-                        {{-- <div class="flex items-center mt-2 text-sm text-gray-500">
-                            <!-- Heroicon name: solid/location-marker -->
-                            <svg class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                <path fill-rule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd" />
-                            </svg>
-                            Remote
-                        </div>
-                        <div class="flex items-center mt-2 text-sm text-gray-500">
-                            <!-- Heroicon name: solid/currency-dollar -->
-                            <svg class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                <path d="M8.433 7.418c.155-.103.346-.196.567-.267v1.698a2.305 2.305 0 01-.567-.267C8.07 8.34 8 8.114 8 8c0-.114.07-.34.433-.582zM11 12.849v-1.698c.22.071.412.164.567.267.364.243.433.468.433.582 0 .114-.07.34-.433.582a2.305 2.305 0 01-.567.267z" />
-                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.511-1.31c-.563-.649-1.413-1.076-2.354-1.253V5z" clip-rule="evenodd" />
-                            </svg>
-                            $120k &ndash; $140k
-                        </div>
-                        <div class="flex items-center mt-2 text-sm text-gray-500">
-                            <!-- Heroicon name: solid/calendar -->
-                            <svg class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd" />
-                            </svg>
-                            Closing on January 9, 2020
-                        </div> --}}
                     </div>
                 </div>
                 <div class="flex mt-5 lg:mt-0 lg:ml-4">
@@ -123,13 +102,14 @@
                         <h4>{{ ucwords(trans_choice('messages.customer_details', 1)) }} </h4>
                         @if(Auth::user()->userlevel->name == "Super Admin" || Auth::user()->userlevel->name == "Provider")
                         <div class="flex items-center mb-4 ml-2 mr-2 text-gray-500">
-                            <!-- Heroicon name: solid/location-marker -->
+                            @if($customer->microsoftTenantInfo->first())
                             <a href="https://partner.microsoft.com/commerce/customers/{{$customer->microsoftTenantInfo->first()->tenant_id}}/subscriptions" target="_blank" class="flex items-center text-sm text-gray-500">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                                 </svg>
                                 to partner center
                             </a>
+                            @endif
                         </div>
                         @endif
                     </div>
@@ -224,7 +204,7 @@
                                         <div class="py-1 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                                             <dt class="text-sm font-medium text-gray-500">{{ ucwords(trans_choice('messages.qualification', 1)) }}</dt>
                                             <dd class="text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{strtoupper($customer->qualification)}}</dd>
-                                            <span class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium {{ $customer->qualification_status == 'messages.Approved' ? 'bg-green-100 text-green-800' : 'bg-yellow-100 text-yellow-800'  }}  capitalize">
+                                            <span class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium {{ $customer->qualification_status == 'Approved' ? 'bg-green-100 text-green-800' : 'bg-yellow-100 text-yellow-800'  }}  capitalize">
                                                 {{$customer->qualification_status}}
                                             </span>
                                         </div>
