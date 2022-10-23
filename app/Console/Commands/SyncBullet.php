31c67c75f59dd2c160bf16d4bdaab80bfdf59df6
joaquim soares <joaquim.soares@tagydes.com>
2022-10-23 12:25:49 +0200

    add command

---
+++ app/Console/Commands/SyncBullet.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\BullethqInvoices;
+use App\Models\BullethqPayments;
+use Illuminate\Support\Facades\Http;
+use AddBullethpuserIdToProvidersTable;
+
+class SyncBullet extends Command
+{
+    /**
+    * The name and signature of the console command.
+    *
+    * @var string
+    */
+    protected $signature = 'command:SyncBullet';
+
+    /**
+    * The console command description.
+    *
+    * @var string
+    */
+    protected $description = 'Command description';
+
+    /**
+    * Execute the console command.
+    *
+    * @return int
+    */
+    public function handle()
+    {
+        $payments = Http::withBasicAuth('joaquim.soares@tagydes.com', '25a142a35cccedbb8465cf148790eefe')
+        ->get('https://accounts-app.bullethq.com/api/v1/clientPayments')->collect();
+
+        $invoices = Http::withBasicAuth('joaquim.soares@tagydes.com', '25a142a35cccedbb8465cf148790eefe')
+        ->get('https://accounts-app.bullethq.com/api/v1/invoices')->collect();
+
+        foreach($invoices as $invoice){
+            BullethqInvoices::updateorcreate([
+                "bullethq_id"   => $invoice['id']
+            ],[
+                "outstandingAmount" => $invoice['outstandingAmount'],
+                "currency"          => $invoice['currency'],
+                "dueDate"           => $invoice['dueDate'],
+                "issueDate"         => $invoice['issueDate'],
+                "clientName"        => $invoice['clientName'],
+                "clientId"          => $invoice['clientId'],
+                "invoiceLines"      => $invoice['invoiceLines'],
+
+            ]);
+
+
+            foreach($payments as $payment){
+                BullethqPayments::updateorcreate([
+                    "bullethq_id"   => $payment['id']
+                ],[
+                    "exchangeRate"  => $payment['exchangeRate'],
+                    "amount"        => $payment['amount'],
+                    "dateReceived"  => $payment['dateReceived'],
+                    "clientId"      => $payment['clientId'],
+                    "invoiceIds"    => $payment['invoiceIds'],
+
+                ]);
+            }
+
+
+
+        }
+    }
+}
